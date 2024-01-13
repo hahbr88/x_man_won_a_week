618c5767148111f68df122d2dc469ad01e612f22
hahbr88 <90291796+hahbr88@users.noreply.github.com>
2024-01-13 23:09:49 +0900

    일주일 예산 입력 input 추가, 기타 스타일 수정, readme 문서 수정

--- popup.js
+++ popup.js
@@ -1,14 +1,33 @@
-function calculateSum(data) {
+const calculateSum = (data) => {
+  const sum = Object.values(data).reduce(
+    (acc, val) => acc + (Number(val) || 0),
+    0
+  );
   return Object.values(data).reduce((acc, val) => acc + (Number(val) || 0), 0);
-}
+};
 
-function updateDisplay(sum) {
-  document.getElementById(
-    "totalSum"
-  ).innerText = `Total: ${sum.toLocaleString()} 원`;
-}
+const updateDisplay = (sum) => {
+  const budget = localStorage.getItem("budget");
+  if (sum > budget) {
+    const excessAmount = sum - budget;
+    document.getElementById(
+      "totalSum"
+    ).innerText = `${budget.toLocaleString()} 원 에서  ${excessAmount.toLocaleString()} 원 초과하였습니다.`;
+  } else {
+    document.getElementById(
+      "totalSum"
+    ).innerText = `총 ${sum.toLocaleString()} 원 사용, 남은 금액 ${(
+      budget - sum
+    ).toLocaleString()} 원`;
+  }
+};
 
-function handleInputChange() {
+const setBudget = () => {
+  const budget = document.getElementById("budget").value;
+  localStorage.setItem("budget", budget);
+};
+
+const handleInputChange = () => {
   const data = {
     monday: document.getElementById("monday").value,
     tuesday: document.getElementById("tuesday").value,
@@ -22,7 +41,9 @@ function handleInputChange() {
   updateDisplay(sum);
 
   localStorage.setItem("weeklyNumbers", JSON.stringify(data));
-}
+};
+
+document.getElementById("budget").addEventListener("change", setBudget);
 
 ["monday", "tuesday", "wednesday", "thursday", "friday"].forEach((day) => {
   document.getElementById(day).addEventListener("change", handleInputChange);
@@ -37,5 +58,6 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const sum = calculateSum(savedData);
+  document.getElementById("budget").value = localStorage.getItem("budget");
   updateDisplay(sum);
 });
